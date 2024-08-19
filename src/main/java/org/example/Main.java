ce654fcbdc53a2ab3b4b1025563787832b2cb0c0
yujihun1 <sk0253642@gmail.com>
2024-08-19 12:25:54 +0900

    1-1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,12 +11,16 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("==게시판 앱==");
-        String inputStrData = sc.nextLine();
-        System.out.printf("명령 : %s\n", inputStrData);
 
-        sc.close();
+        while(true) {
+            System.out.printf("명령)");
+            String command = sc.nextLine();
 
+            if(command.equals("종료")) {
+                break;
+            }
 
+        }
 
     }
 }
