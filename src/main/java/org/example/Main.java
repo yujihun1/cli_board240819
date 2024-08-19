c70a52cacb25132bd745b07b9f24578f02737d2b
yujihun1 <sk0253642@gmail.com>
2024-08-19 12:17:51 +0900

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("HI");
+        System.out.println("==게시판 앱==");
+        String inputStrData = sc.nextLine();
+        System.out.printf("명령 : %s\n", inputStrData);
+
+        sc.close();
 
 
 
