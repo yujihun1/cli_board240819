b82e3ad72445198081c83a09752c74e19501f8f1
yujihun1 <sk0253642@gmail.com>
2024-08-19 13:28:26 +0900

    1-55com

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,2 +1,25 @@
-package org.example;public class Article {
+package org.example;
+
+public class Article {
+
+    private int id;
+    private String subject;
+    private String content;
+
+    Article(int id, String subject, String content) {
+
+        this.id = id;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    public String getSubject(){
+        return this.subject;
+    }
+    public String getContent(){
+        return this.content;
+    }
 }
--- src/main/java/org/example/app.java
+++ src/main/java/org/example/app.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class app {
+public class app  {
     Scanner sc;
 
     app(Scanner sc) {
@@ -12,10 +15,11 @@ public class app {
 
     void run() {
         int lastId=1;
+        List<Article> articleList = new ArrayList<>();
         System.out.println("==게시판 앱==");
 
         while (true) {
-            System.out.println("명령)");
+            System.out.print("명령)");
             String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
@@ -27,17 +31,32 @@ public class app {
 
             }else if(command.equals("등록")){
                 System.out.print("제목:");
-                String subjext = sc.nextLine().trim();
+                String subject = sc.nextLine().trim();
                 System.out.print("내용 :");
                 String content = sc.nextLine().trim();
-                System.out.printf("%d번 게시물이 출력되었습니다.",lastId);
+
+                Article article= new Article(lastId,subject,content);
+                articleList.add(article);
+
+
+
+                System.out.printf("%d번 게시물이 등록되었습니다.\n",lastId);
                 lastId++;
 
 
 
+
+            }else if (command.equals("목록")){
+                System.out.println("번호/ 제목 / 내용");
+                System.out.println("------------------");
+                for(int i=0; i<articleList.size();i++){
+                    Article article = articleList.get(i);
+                    System.out.printf("%d,%s,%s\n",article.getId(),article.getSubject(),article.getContent());
+                }
 
             }
 
         }
     }
 }
+
