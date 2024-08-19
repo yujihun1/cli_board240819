f9c37dda456240c7e2768f6edc9d5bc3ec6b1ed6
yujihun1 <sk0253642@gmail.com>
2024-08-19 12:45:54 +0900

    1-22com

--- src/main/java/org/example/app.java
+++ src/main/java/org/example/app.java
@@ -4,27 +4,38 @@ import java.util.Scanner;
 
 public class app {
     Scanner sc;
+
     app(Scanner sc) {
         this.sc = sc;
     }
 
 
+    void run() {
 
-      void run(){
+        System.out.println("==게시판 앱==");
 
-          System.out.println("==게시판 앱==");
+        while (true) {
+            System.out.printf("명령)");
+            String command = sc.nextLine().trim();
 
-          while(true) {
-              System.out.printf("명령)");
-              String command = sc.nextLine();
+            if (command.equals("종료")) {
+                break;
 
-              if(command.equals("종료")) {
-                  break;
-      }
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
+
+                //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+                // to see how IntelliJ IDEA suggests fixing it.
+
+            }else if(command.equals("등록")){
+                System.out.print("제목:");
+                String subjext = sc.nextLine().trim();
+                System.out.print("내용 :");
+                String content = sc.nextLine().trim();
+
+
+
+
+            }
 
         }
-
     }
 }
